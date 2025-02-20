cfeba1293c3f463551bb2b33c18aee850244e0ce
Colt33 <wellingtonrcampos@hotmail.com>
2025-02-20 18:33:42 -0300

    Aula01 e 02

---
+++ src/Aula01_02.java
@@ -0,0 +1,14 @@
+public class Aula01_02 {
+    /** 01   O que é Git?
+     * Git é um Software de versão de projetos
+     * | ---> Controle de Histórico
+     * | ---> Trabalhar em Equipe no mesmo Projeto
+     * | ---> Logs de Trabalhos
+     * | ---> Trabalhar de forma isolada em seu proprio ambiente
+     * | ---> Eliminar sujeira em código e duplicidade
+     * **/
+
+    /** 02 O que é GitHub?
+     * GitHub é uma Plataforma de Repositório remoto/Rede Social de Devs e para Portifolio
+     * **/
+}
