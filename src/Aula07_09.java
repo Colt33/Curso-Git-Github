6d806e1706646025a04dfb6b627ddff6f379a9b5
Colt33 <wellingtonrcampos@hotmail.com>
2025-02-21 20:41:58 -0300

    aula07-09

---
+++ src/Aula07_09.java
@@ -0,0 +1,23 @@
+public class Aula07_09 {
+    /**Aula07
+     * Salvando Commit
+     * Vai no terminal da IDE veja se a pasta foi iniciada
+     * como repositório git usando o comando git status.
+     * Se não foi iniciado use o comando git init para iniciar.
+     *
+     * Para adicionar o requerimento desejato para fazer o commit use
+     * o comando com o nome da pasta e arquivo.java
+     * ex: git add src/Aula07_09.java ou Ctrl+Alt+A (ou git add . se for todos do projeto).
+     *
+     * Use o comando git commit ex: git commit src/Aula07_09.java -m""
+     * (-m"" se vc for comentar sobre o commit)
+     * **/
+
+    /**Aula09
+     * Enviando Repositório no GitHub
+     *
+     * Comando git branch -M main
+     * git remote add origin git@github.com:Colt33/Curso-Git-Github.git
+     * git push -u origin main
+     * **/
+}
