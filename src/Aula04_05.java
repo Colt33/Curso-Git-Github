9c0e6f698765e2f24ff02e8905292ff1ba967fce
Colt33 <wellingtonrcampos@hotmail.com>
2025-02-21 20:42:39 -0300

    aula04-05

---
+++ src/Aula04_05.java
@@ -0,0 +1,21 @@
+public class Aula04_05 {
+    /**Aula04
+     * O que é um Repositório?
+     * É onde os arquivos do seu projeto ficam armazenados.
+     *
+     * Quando iniciamos um projeto git, estamos na verdade
+     * criando um repositorio para aquele projeto.
+     *
+     * Você pode enviar o repositório para alguns servidores
+     * específicos para gerênciar repositórios como o GitHub, Bitbucket ou GitLab
+     * **/
+    /**Aula05
+     * Iniciando um Repositório
+     * Usamos o comando: git init
+     * Com isso você está iniciando um repositório dessa
+     * pasta que digitou git init
+     *
+     * Todos os arquivos dentro dessa pasta agora serão
+     * reconhecidos pelo git como um projeto
+     * **/
+}
