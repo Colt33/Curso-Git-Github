55503e71800178a7ac43c32db665d9c82b3898be
Colt33 <wellingtonrcampos@hotmail.com>
2025-02-22 11:19:02 -0300

    Ultima aula de criação/git add/ git commit e git push

---
+++ src/Aula10.java
@@ -0,0 +1,15 @@
+public class Aula10 {
+    /**Aula10
+     * Lembrando que ao criar novos arquivos .java é preciso usar o comando
+     * git add . para adicionar todos os arquivos novos ou colocar git add o nome da
+     * pasta/nome do arquivo.java
+     *
+     * Verifica se foi adicionado a pasta com o comando git status
+     *
+     * Após verificar use o comando git commit para fazer o commit
+     * ex; git commit src/Aula10.java -m"quando for fazer commit de um arquivo" /
+     * git commit -m"quando for para comitar todos os arquivos que você alterou e deu git add"
+     *
+     * em seguida usar o comando git push ele vai enviar para o repositório remoto
+     * **/
+}
