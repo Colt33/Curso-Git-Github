2ddb456b96bc8301ff279b6bbef4ebc0f9f9fec1
Colt33 <wellingtonrcampos@hotmail.com>
2025-02-24 16:03:16 -0300

    Como clonar um repositório do GitHub

---
+++ parte02_curso/src/Aula12_ClonandoRepositorios.java
@@ -0,0 +1,10 @@
+public @interface Aula12_ClonandoRepositorios {
+    /**Aula12
+     * Clonando Repositórios
+     * para clonar um repositorio que está presente no git
+     * basta criar uma pasta abrir o Git Bash e colocar o comando
+     * git clone + o HTTPS do repositório do GitHub
+     * ex: git clone https://github.com/Colt33/Curso-Git-Github.git
+     * **/
+
+}
